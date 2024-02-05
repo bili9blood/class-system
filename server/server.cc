e9032b79c493546ce060b561ab78a983520c030e
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-02-05 20:01:12 +0800

    feat: handle request

--- server/server.cc
+++ server/server.cc
@@ -3,6 +3,8 @@
 #include <hv/hlog.h>
 #include <proto/Request.pb.h>
 
+#include "logic.h"
+
 Server::Server(unsigned short _port, int thread_num) {
   port = _port;
 
@@ -29,4 +31,7 @@ void Server::OnConnection(const hv::SocketChannelPtr& chn) const {
 
 void Server::OnMessage(const hv::SocketChannelPtr& chn, hv::Buffer* buf) const {
   LOGI("recv a message from %s, len=%llu", chn->peeraddr().c_str(), buf->size() - unpack_setting_.body_offset);
+
+  const auto resp = logic::HandleRequest(buf);
+  chn->write(resp.get());
 }
