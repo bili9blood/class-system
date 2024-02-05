5da96c28a1768290ccf5ffa648883f69f9ed3b48
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-02-05 19:01:28 +0800

    refactor: 抽离callback为成员函数

--- server/server.cc
+++ server/server.cc
@@ -1,24 +1,32 @@
 #include "server.h"
 
+#include <hv/hlog.h>
 #include <proto/Request.pb.h>
 
 Server::Server(unsigned short _port, int thread_num) {
   port = _port;
 
-  unpack_setting_t unpack_setting;
-  unpack_setting.body_offset         = 4;
-  unpack_setting.length_field_offset = 0;
-  unpack_setting.length_field_bytes  = 4;
-  setUnpack(&unpack_setting);
-
-  onMessage = [](const hv::SocketChannelPtr& channel, hv::Buffer* buf) {
-    class_system::Request req{};
-    // NOLINTNEXTLINE
-    if (req.ParseFromArray((char*)buf->data() + 4, (int)buf->size() - 4))
-      req.PrintDebugString();
-    else
-      printf("error\n");
-  };
+  unpack_setting_.body_offset         = 4;
+  unpack_setting_.length_field_offset = 0;
+  unpack_setting_.length_field_bytes  = 4;
+  setUnpack(&unpack_setting_);
 
   setThreadNum(thread_num);
+
+  onConnection = std::bind(
+      &Server::OnConnection,
+      this, std::placeholders::_1
+  );
+  onMessage = std::bind(
+      &Server::OnMessage,
+      this, std::placeholders::_1, std::placeholders::_2
+  );
+}
+
+void Server::OnConnection(const hv::SocketChannelPtr& chn) const {
+  LOGI("client %s %s", chn->peeraddr().c_str(), chn->isConnected() ? "connected" : "disconnected");
+}
+
+void Server::OnMessage(const hv::SocketChannelPtr& chn, hv::Buffer* buf) const {
+  LOGI("recv a message from %s, len=%llu", chn->peeraddr().c_str(), buf->size() - unpack_setting_.body_offset);
 }
--- server/server.h
+++ server/server.h
@@ -5,4 +5,11 @@
 class Server : public hv::TcpServer {
  public:
   Server(unsigned short port, int thread_num);
+
+ private:
+  unpack_setting_t unpack_setting_;
+
+ protected:
+  void OnConnection(const hv::SocketChannelPtr& chn) const;
+  void OnMessage(const hv::SocketChannelPtr& chn, hv::Buffer* buf) const;
 };
