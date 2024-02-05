043a3350197082064215f0029a5a7561d5ecc7b9
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-02-05 18:27:01 +0800

    feat: load configurations from ini

--- server/main.cc
+++ server/main.cc
@@ -1,8 +1,21 @@
+#include <hv/iniparser.h>
+
 #include "server.h"
 
 auto main() -> int {
-  Server srv{8979, 4};
+  IniParser ini;
+  ini.LoadFromFile("class-system-server.ini");
+  const unsigned short port       = ini.Get("port", "", 7989);
+  const int            thread_num = ini.Get("thread_num", "", 1);
+
+  // init server with configurations
+  Server srv{port, thread_num};
+
+  // unload iniparser after initializing server
+  ini.Unload();
 
   srv.start();
+
+  // main-thread loop
   while (true) hv_msleep(200);
 }
