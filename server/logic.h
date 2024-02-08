4493f25eb0f9e309b00ad23858b8f55b349bcb6a
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-02-08 10:48:14 +0800

    docs: add doxygen comments

--- server/logic.h
+++ server/logic.h
@@ -4,6 +4,12 @@
 
 namespace logic {
 
+/**
+ * @brief Handle the request from client and return a response
+ *
+ * @param req the request from client
+ * @return hv::BufferPtr response buffer
+ */
 hv::BufferPtr HandleRequest(hv::Buffer* req);
 
 }
--- server/util.h
+++ server/util.h
@@ -5,6 +5,12 @@
 
 namespace util {
 
+/**
+ * @brief Convert protobuf message to hv::BufferPtr (aka std::shared_ptr<hv::Buffer>)
+ *
+ * @param msg the protobuf message
+ * @return hv::BufferPtr the buffer after converting, never return an empty buffer ptr!
+ */
 hv::BufferPtr MessageToBuf(const google::protobuf::MessageLite& msg);
 
 }
