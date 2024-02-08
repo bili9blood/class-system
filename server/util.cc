caf1de7681c4cec96635d4cb9a3bd39e0e6c7fef
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-02-08 10:45:32 +0800

    fix: `MessageToBuf`return an empty buffer

--- server/util.cc
+++ server/util.cc
@@ -1,15 +1,24 @@
 #include "util.h"
 
+#ifdef max
+#undef max
+#endif
+
 namespace util {
 
 hv::BufferPtr MessageToBuf(const google::protobuf::MessageLite& msg) {
   std::string str;
   msg.SerializeToString(&str);
 
-  auto* byte_arr = new char[str.size()];  // will be freed by hv::Buffer
-  memcpy(byte_arr, str.c_str(), str.size());
+  void* byte_arr{};  // will be freed by hv::Buffer
+  if (str.empty()) {
+    byte_arr = new char[1];
+  } else {
+    byte_arr = new char[str.size()];
+    memcpy(byte_arr, str.c_str(), str.size());
+  }
 
-  return std::make_shared<hv::Buffer>(byte_arr, str.size());
+  return std::make_shared<hv::Buffer>(byte_arr, std::max(1ull, str.size()));
 }
 
 }  // namespace util
