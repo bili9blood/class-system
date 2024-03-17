951bb876d4190987f624bc3db67acd9c54b9faaf
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-03-17 09:43:33 +0800

    refactor: move `TheHeader`

---
+++ manage/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { appName } from "~/constants";
+import IconIco from "~/resources/images/icon.ico";
+</script>
+
+<template>
+  <q-header
+    height-hint="98"
+    elevated
+    text-primary-content
+  >
+    <q-toolbar>
+      <q-toolbar-title>
+        <q-avatar square>
+          <img :src="IconIco">
+        </q-avatar>
+        {{ appName }}
+      </q-toolbar-title>
+
+      <q-tabs align="right" hidden sm:block>
+        <q-route-tab to="/">
+          主页
+        </q-route-tab>
+        <q-route-tab to="/students">
+          学生
+        </q-route-tab>
+      </q-tabs>
+    </q-toolbar>
+  </q-header>
+</template>
--- manage/layouts/default.vue
+++ manage/layouts/default.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { appName } from "~/constants";
-import IconIco from "~/resources/images/icon.ico";
-
 const bgImgUrl: string = JSON.parse(await $fetch("/api/dailyImage", { headers: { "Accept-Encoding": "none" } })).data;
 </script>
 
@@ -15,29 +12,7 @@ const bgImgUrl: string = JSON.parse(await $fetch("/api/dailyImage", { headers: {
     </div>
 
     <q-layout flex="~ col" h-full>
-      <q-header
-        height-hint="98"
-        elevated
-        text-primary-content
-      >
-        <q-toolbar>
-          <q-toolbar-title>
-            <q-avatar square>
-              <img :src="IconIco">
-            </q-avatar>
-            {{ appName }}
-          </q-toolbar-title>
-
-          <q-tabs align="right" hidden sm:block>
-            <q-route-tab to="/">
-              主页
-            </q-route-tab>
-            <q-route-tab to="/students">
-              学生
-            </q-route-tab>
-          </q-tabs>
-        </q-toolbar>
-      </q-header>
+      <the-header />
 
       <q-page-container flex-1>
         <div h-full w-full flex>
