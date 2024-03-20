07dbcf90b98be158b582261ec693d7869a728e82
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-03-20 19:49:09 +0800

    refactor(display): config.cc

---
+++ display/config.cc
@@ -0,0 +1,52 @@
+#pragma once
+#include "config.h"
+
+#include <qapplication.h>
+#include <qsavefile.h>
+
+#include <sstream>
+
+static std::optional<toml::table> cfg;
+static config::AutoSaveConfig     auto_save_cfg;
+
+namespace config {
+
+AutoSaveConfig::~AutoSaveConfig() {
+  if (qApp) Save();
+}
+toml::table&                AutoSaveConfig::operator->() { return *cfg; }
+toml::node_view<toml::node> AutoSaveConfig::operator[](const char* k) { return (*cfg)[k]; }
+
+AutoSaveConfig              Get() {
+  if (!cfg) {
+    cfg = toml::parse_file(GetPath().toStdString());
+
+#define CONFIG_INSERT(name) \
+  if (!cfg->contains(#name)) cfg->insert(#name, toml::table{})
+
+    CONFIG_INSERT(DisplayWindow);
+    CONFIG_INSERT(IconWindow);
+    CONFIG_INSERT(ExtraWindow);
+
+#undef CONFIG_INSERT
+  }
+
+  return {auto_save_cfg};
+}
+
+void Save() {
+  if (!cfg) return;
+
+  QSaveFile file{GetPath()};
+  file.open(QIODevice::WriteOnly);
+
+  std::stringstream ss;
+  ss << toml::toml_formatter{*cfg};
+
+  file.write(ss.str().c_str());
+  file.commit();
+}
+
+QString GetPath() { return QApplication::applicationDirPath() + "/config.toml"; }
+
+}  // namespace config
--- display/config.h
+++ display/config.h
@@ -1,57 +1,17 @@
 #pragma once
-#include <qapplication.h>
-#include <qsavefile.h>
-
-#include <sstream>
 #include <toml.hpp>
 
 namespace config {
 
-void Save();
-
-namespace _D {
-inline std::optional<toml::table> cfg;
-
 class AutoSaveConfig {
  public:
-  ~AutoSaveConfig() {
-    if (qApp) config::Save();
-  }
-  toml::table& operator->() { return *cfg; }
-  auto         operator[](const char* k) { return (*cfg)[k]; }
-} inline auto_save_cfg;
+  ~AutoSaveConfig();
+  toml::table&                operator->();
+  toml::node_view<toml::node> operator[](const char* k);
+};
 
-}  // namespace _D
+class AutoSaveConfig Get();
+void                 Save();
+QString              GetPath();
 
-inline QString            GetPath() { return QApplication::applicationDirPath() + "/config.toml"; }
-
-inline _D::AutoSaveConfig Get() {
-  if (!_D::cfg) {
-    _D::cfg = toml::parse_file(GetPath().toStdString());
-
-#define CONFIG_INSERT(name) \
-  if (!_D::cfg->contains(#name)) _D::cfg->insert(#name, toml::table{})
-
-    CONFIG_INSERT(DisplayWindow);
-    CONFIG_INSERT(IconWindow);
-    CONFIG_INSERT(ExtraWindow);
-
-#undef CONFIG_INSERT
-  }
-
-  return {_D::auto_save_cfg};
-}
-
-inline void Save() {
-  if (!_D::cfg) return;
-
-  QSaveFile file{GetPath()};
-  file.open(QIODevice::WriteOnly);
-
-  std::stringstream ss;
-  ss << toml::toml_formatter{*_D::cfg};
-
-  file.write(ss.str().c_str());
-  file.commit();
-}
 }  // namespace config
