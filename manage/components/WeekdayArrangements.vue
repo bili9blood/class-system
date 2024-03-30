fb044e905bb91f8d073ee2e2d3791bdc8d402844
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-03-30 17:42:43 +0800

    chore(manage/arrangement): able to scroll table

--- manage/components/WeekdayArrangements.vue
+++ manage/components/WeekdayArrangements.vue
@@ -19,39 +19,43 @@ function HandleEditArr(idx: number) {
 </script>
 
 <template>
-  <q-btn icon="add" outline @click="HandleInsertArr(-1)">
-    添加工作日安排
-  </q-btn>
-  <q-markup-table separator="cell" flat min-w-160 justify-center bg-transparent>
-    <thead font-bold>
-      <tr>
-        <th>职位</th>
-        <th>今日安排</th>
-        <th>操作</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="weekday_arr, idx in info?.weekday_arr" :key="weekday_arr.id">
-        <td justify-center>
-          <q-input v-model="weekday_arr.job" outlined dense />
-        </td>
+  <q-card-section>
+    <q-btn icon="add" outline @click="HandleInsertArr(-1)">
+      添加工作日安排
+    </q-btn>
+  </q-card-section>
+  <q-card-section w-full overflow-x-auto>
+    <q-markup-table separator="cell" flat min-w-160 justify-center bg-transparent>
+      <thead font-bold>
+        <tr>
+          <th>职位</th>
+          <th>今日安排</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="weekday_arr, idx in info?.weekday_arr" :key="weekday_arr.id">
+          <td justify-center>
+            <q-input v-model="weekday_arr.job" outlined dense />
+          </td>
 
-        <td>
-          <div flex="~ gap-2">
-            <div v-for="daily in DailyArrFromWeekday(weekday_arr)" :key="daily">
-              {{ StudentDisplayString(info?.students.find(s => s.inclass_id === daily)) }}
+          <td>
+            <div flex="~ gap-2">
+              <div v-for="daily in DailyArrFromWeekday(weekday_arr)" :key="daily">
+                {{ StudentDisplayString(info?.students.find(s => s.inclass_id === daily)) }}
+              </div>
             </div>
-          </div>
-        </td>
+          </td>
 
-        <td>
-          <div flex justify-center>
-            <q-btn flat dense label="编辑" @click="HandleEditArr(idx)" />
-            <q-btn flat dense label="删除" @click="HandleRemoveArr(idx)" />
-            <q-btn flat dense label="插入" @click="HandleInsertArr(idx)" />
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </q-markup-table>
+          <td>
+            <div flex justify-center>
+              <q-btn flat dense label="编辑" @click="HandleEditArr(idx)" />
+              <q-btn flat dense label="删除" @click="HandleRemoveArr(idx)" />
+              <q-btn flat dense label="插入" @click="HandleInsertArr(idx)" />
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </q-card-section>
 </template>
--- manage/pages/arrangements.vue
+++ manage/pages/arrangements.vue
@@ -7,9 +7,7 @@ definePageMeta({
 <template>
   <div>
     <q-card bg-secondary text-black>
-      <q-card-section>
-        <weekday-arrangements />
-      </q-card-section>
+      <weekday-arrangements />
     </q-card>
   </div>
 </template>
