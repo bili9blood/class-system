b52117eed9bfc260c5b29ca6a6009c9868b4e08c
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-03-30 15:38:21 +0800

    fix(manage/utils): return empty array when not on weekdays

--- manage/utils/arrangements.ts
+++ manage/utils/arrangements.ts
@@ -1,5 +1,6 @@
 import type { CompleteArrangement, PartialArrangement, WeekdayArrangement } from "~/type";
 
 export function DailyArrFromWeekday(arr: WeekdayArrangement) {
-  return arr.student_ids[(moment().weekday() - 1) % 5];
+  const w = moment().weekday();
+  return w - 1 < 5 ? arr.student_ids[(w - 1) % 5] : [];
 }
