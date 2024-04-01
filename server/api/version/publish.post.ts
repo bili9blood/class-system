138974c84453adcef8120cfacd559b34ca870717
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-04-01 20:56:29 +0800

    feat(server): publish  version

--- server/api/version/publish.post.ts
+++ server/api/version/publish.post.ts
@@ -1,9 +1,12 @@
 import process from "node:process";
+import { fetchLatest } from "gh-release-fetch";
 
 const partsParser = z.object({
   token: z.string(),
   version: z.string().regex(/v(\d+\.){2}\d+/),
 });
+
+const file_name = "class-system-display_windows-x86_64.zip";
 
 export default defineEventHandler(async (e) => {
   const parsed = partsParser.safeParse(await readBody(e));
@@ -14,6 +17,23 @@ export default defineEventHandler(async (e) => {
 
   if (token !== process.env.PUBLISH_TOKEN)
     return createResp(null, 401, "Token 错误！");
+
+  await fetchLatest(
+    {
+      repository: "bili9blood/class-system",
+      package: file_name,
+      destination: "",
+      version,
+      extract: false,
+    },
+  );
+
+  const form_data = new FormData();
+  form_data.append("file", new File([file_name], file_name));
+
+  const res = await fetch(`https://upload.kstore.space/upload/0?access_token=${token}`, { body: form_data });
+  if (res.status !== 200)
+    return createResp(null, 500, "上传文件失败！");
 
   const db = useDatabase();
   try {
