a73b5693a275aebc238d3ee658888418776b9b6f
9Blood <62489989+bili9blood@users.noreply.github.com>
2024-05-05 17:21:58 +0800

    feat(display): display version

--- display/titlewidget.cc
+++ display/titlewidget.cc
@@ -1,9 +1,12 @@
 #include "titlewidget.h"
 
 #include "ui_titlewidget.h"
+#include "version.h"
 
 TitleWidget::TitleWidget(QWidget *parent) : QWidget{parent}, ui_{new Ui::TitleWidget} {
   ui_->setupUi(this);
+
+  ui_->title_label->setText(QString::asprintf(ui_->title_label->text().toUtf8().data(), kVersion + 1));
 }
 
 TitleWidget::~TitleWidget() = default;
